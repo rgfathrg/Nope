9c6c6f1e4f9fbea736fe3ccd40e63487a399859c
Michael Fleshman <michaelfleshman90@gmail..com>
2018-10-17 17:25:03 -0400

    jjj

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,12 @@
-var db = require("../models");
+var db = require("../models/flickr.js");
+var express = require("express");
+var router = express.router();
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
+
+router.get("/flickr", function(req, res) {
+  /services/rest/?method=flickr.galleries.getPhoto&api_key=1748d065e98eb8052cc29c44d00fbdec
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
-  });
+});
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
