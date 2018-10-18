a429d963fa779be90128ef27ca96409d46bf770d
courtneyduggins <courtney.duggins@gmail.com>
2018-10-17 21:05:29 -0400

    add weather ajax

--- api/weather.js
+++ api/weather.js
@@ -1,2 +1,11 @@
+var weatherAPIKey = "f5f08ce79b534c1a907162145181310";
+var queryURL = "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=" + weatherAPIKey + "&q=Charlotte&format=JSON";
+// var location = ""
 
-
+$.ajax({
+url: queryURL,
+method: "GET"
+}).then(function(response){
+console.log(response);
+console.log("It works, see... " + response.data.current_condition[0].observation_time);
+});
