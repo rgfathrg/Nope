353090262ffb9130691a616579317a9eb9f46065
Michael Fleshman <michaelfleshman90@gmail..com>
2018-10-17 20:54:55 -0400

    jjj

---
+++ api/flickr.js
@@ -0,0 +1,30 @@
+// var Flickr = require("flickrapi"),
+//     flickrOptions = {
+//         api_key: "1748d065e98eb8052cc29c44d00fbec",
+//         secret: "efaefac1c0792b40"
+//     };
+// Flickr.authenticate(flickrOptions, function(err, flickr) { 
+//     flickr.photos.search({
+//         text: "red+panda"
+//       }, function(err, result) {
+//         if(err) { throw new Error(err); }
+//         // do something with result
+//       }
+// });
+// ES Modules syntax
+import Unsplash from 'unsplash-js';
+
+// require syntax
+const Unsplash = require('unsplash-js').default;
+
+const unsplash = new Unsplash({
+  applicationId: "4772a941e9725925f6aa568234bc3738e8fc1edd78f297d9c2a7b433a05719ed",
+  secret: "07705540f46dd1ba46d7e92a923840baac9cef9d56fbf2b06f6cea9fc1717a21",
+  callbackUrl: "{CALLBACK_URL}"
+});
+
+unsplash.photos.getPhoto("mtNweauBsMQ", 1920, 1080, [0, 0, 1920, 1080])
+  .then(toJson)
+  .then(json => {
+      console.log(json);
+  });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
-var db = require("../models/flickr.js");
-var express = require("express");
-var router = express.router();
+//var db = require("../models/flickr.js");
+// var express = require("express");
 
 
-router.get("/flickr", function(req, res) {
-  /services/rest/?method=flickr.galleries.getPhoto&api_key=1748d065e98eb8052cc29c44d00fbdec
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-});
+
+// app.get("/flickr", function(req, res) {
+//   //services/rest/?method=flickr.galleries.getPhoto&api_key=1748d065e98eb8052cc29c44d00fbdec
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+// });
 
